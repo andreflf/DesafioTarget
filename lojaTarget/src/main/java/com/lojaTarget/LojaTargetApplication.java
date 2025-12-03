f01426d35c7d96fa83da9a68c8a2833649ced4f7
andreflf <andreferreira.lima@hotmail.com>
2025-12-03 16:28:13 -0300

    chore: primeiro commit com estrutura inicial do projeto

---
+++ lojaTarget/src/main/java/com/lojaTarget/LojaTargetApplication.java
@@ -0,0 +1,13 @@
+package com.lojaTarget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaTargetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaTargetApplication.class, args);
+	}
+
+}
---
+++ lojaTarget/src/test/java/com/lojaTarget/LojaTargetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lojaTarget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaTargetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
